bf25c93faade0c65ed24f48ed3603bfd4c4706b7
Vivek Kumar <vivekthakur1288@gmail.com>
2024-09-11 13:37:07 +0530

    update notification component

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Notification = (props) => {
   const display = props.show?"block":"hidden"
-  const bgcolor = props.type==="success"?"bg-[#00CCDD]":"bg-[#C96868]"
+  const bgcolor = props.type==="success"?"bg-[#7EACB5]":"bg-[#C96868]"
   return (
       <div className={` ${display}  p-4 font-sans text-[15px] ${bgcolor} font-bold text-white animate-fade-up mt-[-100px] rounded-md`}>
       {props.msg}
